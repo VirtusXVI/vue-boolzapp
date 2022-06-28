94753a76aba0dc76068d36f9f7be80e198e16471
VirtusXVI <manuel.aveta2001@gmail.com>
2022-06-28 18:00:30 +0200

    completed structure

---
+++ js/script.js
@@ -0,0 +1,79 @@
+var app = new Vue (
+    {
+        el: "#root",
+        data:{
+            contacts:[
+                {
+                    img: "img/avatar_1.jpg",
+                    name: "Michele",
+                },
+                {
+                    img: "img/avatar_2.jpg",
+                    name: "Giorgio",
+                },
+                {
+                    img: "img/avatar_3.jpg",
+                    name: "Paolo",
+                },
+                {
+                    img: "img/avatar_4.jpg",
+                    name: "Luca",
+                },
+                {
+                    img: "img/avatar_5.jpg",
+                    name: "Diego",
+                },
+                {
+                    img: "img/avatar_6.jpg",
+                    name: "Giovanna",
+                },
+                {
+                    img: "img/avatar_7.jpg",
+                    name: "Peppe",
+                },
+                {
+                    img: "img/avatar_8.jpg",
+                    name: "Franco",
+                },
+                {
+                    img: "img/avatar_1.jpg",
+                    name: "Michele",
+                },
+                {
+                    img: "img/avatar_2.jpg",
+                    name: "Giorgio",
+                },
+                {
+                    img: "img/avatar_3.jpg",
+                    name: "Paolo",
+                },
+                {
+                    img: "img/avatar_4.jpg",
+                    name: "Luca",
+                },
+                {
+                    img: "img/avatar_5.jpg",
+                    name: "Diego",
+                },
+                {
+                    img: "img/avatar_6.jpg",
+                    name: "Giovanna",
+                },
+                {
+                    img: "img/avatar_7.jpg",
+                    name: "Peppe",
+                },
+                {
+                    img: "img/avatar_8.jpg",
+                    name: "Franco",
+                },
+            ]
+        },
+        methods:{
+
+        },
+        mounted(){
+            
+        }
+    }
+)
