3dd932c7b4455cd566b659fef6cba41aa2128fa8
VirtusXVI <manuel.aveta2001@gmail.com>
2022-06-29 13:14:24 +0200

    changed array of contacts

--- js/script.js
+++ js/script.js
@@ -2,72 +2,92 @@ var app = new Vue (
     {
         el: "#root",
         data:{
-            contacts:[
+            contacts: [
                 {
-                    img: "img/avatar_1.jpg",
-                    name: "Michele",
+                    name: 'Michele',
+                    avatar: '_1',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Ricordati di dargli da mangiare',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            text: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
                 },
                 {
-                    img: "img/avatar_2.jpg",
-                    name: "Giorgio",
+                    name: 'Fabio',
+                    avatar: '_2',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '20/03/2020 16:30:00',
+                            text: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            text: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:00',
+                            text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'sent'
+                        }
+                    ],
                 },
                 {
-                    img: "img/avatar_3.jpg",
-                    name: "Paolo",
+                    name: 'Samuele',
+                    avatar: '_3',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '28/03/2020 10:10:40',
+                            text: 'La Marianna va in campagna',
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            text: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            text: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ],
                 },
                 {
-                    img: "img/avatar_4.jpg",
-                    name: "Luca",
-                },
-                {
-                    img: "img/avatar_5.jpg",
-                    name: "Diego",
-                },
-                {
-                    img: "img/avatar_6.jpg",
-                    name: "Giovanna",
-                },
-                {
-                    img: "img/avatar_7.jpg",
-                    name: "Peppe",
-                },
-                {
-                    img: "img/avatar_8.jpg",
-                    name: "Franco",
-                },
-                {
-                    img: "img/avatar_1.jpg",
-                    name: "Michele",
-                },
-                {
-                    img: "img/avatar_2.jpg",
-                    name: "Giorgio",
-                },
-                {
-                    img: "img/avatar_3.jpg",
-                    name: "Paolo",
-                },
-                {
-                    img: "img/avatar_4.jpg",
-                    name: "Luca",
-                },
-                {
-                    img: "img/avatar_5.jpg",
-                    name: "Diego",
-                },
-                {
-                    img: "img/avatar_6.jpg",
-                    name: "Giovanna",
-                },
-                {
-                    img: "img/avatar_7.jpg",
-                    name: "Peppe",
-                },
-                {
-                    img: "img/avatar_8.jpg",
-                    name: "Franco",
+                    name: 'Luisa',
+                    avatar: '_4',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
                 },
             ]
+            
         },
         methods:{
 
